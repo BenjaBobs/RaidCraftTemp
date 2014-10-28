a7a33171beea578acf578c3d3974ea93de541d8e
Benjamin <bhuber11@student.aau.dk>
2014-10-28 13:53:05 +0100

    stuff

---
+++ RaidCraft/Assets/Code/Game/Buildings/BuildGrid.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuildGrid : MonoBehaviour {
+
+    public static BuildGrid staticRef;
+    
+    #region Fields
+    [SerializeField]
+    int _xMax;
+    [SerializeField]
+    int _yMax;
+    #endregion
+
+    BuildTile[,] tiles;
+
+    void Awake()
+    {
+        staticRef = this;
+        tiles = new BuildTile[_xMax,_yMax];
+    }
+
+    void Start()
+    {
+    }
+
+    public static void RegisterTile(BuildTile tile, int x, int y)
+    {
+        if (x >= 0 && x < staticRef._xMax)
+            if (y >= 0 && y < staticRef._yMax)
+                staticRef.tiles[x, y] = tile;
+    }
+
+    public static void SetBuildMode(bool buildMode)    
+    {
+        for (int i = 0; i < staticRef._xMax; i++)
+        {
+            for (int j = 0; j < staticRef._yMax; j++)
+            {
+                if (staticRef.tiles[i, j] != null)
+                    staticRef.tiles[i, j].BuildMode = buildMode;
+            }
+
+        }
+    }
+        
+    
+    
+}
---
+++ RaidCraft/Assets/Code/Game/Buildings/BuildTile.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Collider))]
+public class BuildTile : MonoBehaviour
+{
+
+    #region Fields
+    [SerializeField]
+    Material OccupiedMaterial;
+    [SerializeField]
+    Material VacantMaterial;
+    [SerializeField]
+    Material NormalMaterial;
+
+    int _xCoord;
+    int _yCoord;
+    
+    Renderer rend;
+    bool _buildMode;
+    #endregion
+
+    public int YCoord
+    {
+        get { return _yCoord; }
+        set { _yCoord = value; }
+    }
+
+    public int XCoord
+    {
+        get { return _xCoord; }
+        set { _xCoord = value; }
+    }
+
+    public bool BuildMode
+    {
+        get { return _buildMode; }
+        set 
+        { 
+            _buildMode = value; 
+            if (value == true)
+            {
+                rend.material = VacantMaterial;
+            }
+            else
+            {
+                rend.material = NormalMaterial;
+            }
+        }
+    }
+
+    
+
+    void Awake()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        BuildGrid.RegisterTile(this, 0, 0);
+        rend = renderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            BuildMode = !BuildMode;
+        }
+	    
+	}
+
+    
+}
